7fab9bd34e251e04d75d9065a3ad224a334efe58
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2019-05-22 13:16:44 +0300

    Fix null headers

--- MailhogClient/Models/Headers.cs
+++ MailhogClient/Models/Headers.cs
@@ -1,18 +1,43 @@
-﻿namespace Mailhog.Models
+﻿using Newtonsoft.Json;
+
+namespace Mailhog.Models
 {
     public class Headers
     {
+        [JsonProperty(PropertyName = "Content-Id")]
         public string[] ContentId { get; set; }
+
+        [JsonProperty(PropertyName = "Content-Disposition")]
         public string[] ContentDisposition { get; set; }
+
+        [JsonProperty(PropertyName = "Content-Transfer-Encoding")]
         public string[] ContentTransferEncoding { get; set; }
+
+        [JsonProperty(PropertyName = "Content-Type")]
         public string[] ContentType { get; set; }
+
+        [JsonProperty(PropertyName = "Date")]
         public string[] Date { get; set; }
+
+        [JsonProperty(PropertyName = "From")]
         public string[] From { get; set; }
+
+        [JsonProperty(PropertyName = "MIME-Version")]
         public string[] MIMEVersion { get; set; }
+
+        [JsonProperty(PropertyName = "Message-Id")]
         public string[] MessageId { get; set; }
+
+        [JsonProperty(PropertyName = "Received")]
         public string[] Received { get; set; }
+
+        [JsonProperty(PropertyName = "Return-Path")]
         public string[] ReturnPath { get; set; }
+
+        [JsonProperty(PropertyName = "Subject")]
         public string[] Subject { get; set; }
+
+        [JsonProperty(PropertyName = "To")]
         public string[] To { get; set; }
     }
 }
